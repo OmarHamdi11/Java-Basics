d2a186c1166e5f52d57784a499475c9d2b6344ce
OmarHamd11 <omarellafy1@gmail.com>
2025-03-02 11:57:51 +0200

    hashmap

---
+++ 30 - CollectionFramework/HashMap/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        HashMap<Integer,String> tm = new HashMap<>(Map.of(1,"omar",2,"ali",3,"ahmed",4,"osman"));
+
+        System.out.println(tm.get(4));
+
+        System.out.println(tm);
+    }
+}
